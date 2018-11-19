8193449bdb553478838a98fcc0ad02f1be7c2e48
Jutho <jutho.haegeman@ugent.be>
2018-11-19 12:45:10 +0100

    update travis and docs to accommodate for documenter 0.2

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,6 @@ using Documenter
 using KrylovKit
 
 makedocs(modules=[KrylovKit],
-            format=:html,
-            sitename="KrylovKit.jl",
             pages = [
                 "Home" => "index.md",
                 "Manual" => [
@@ -17,13 +15,4 @@ makedocs(modules=[KrylovKit],
                 ]
             ])
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    deps = nothing,
-    make = nothing,
-    target = "build",
-    repo = "github.com/Jutho/KrylovKit.jl.git",
-    julia = "1.0"
-)
+deploydocs(repo = "github.com/Jutho/KrylovKit.jl.git")
