fae498a1a85a8ce2e553e9818a494c9b2270896f
lkdvos <lukas.devos@ugent.be>
2022-04-28 00:30:57 +0200

    AD rules for eigsolve Lanczos seems to be fine Arnoldi WIP, still needs correct sorting for left eigenvectors

---
+++ src/adrules/eigsolve.jl
@@ -0,0 +1,233 @@
+function ChainRulesCore.rrule(
+    ::typeof(eigsolve),
+    A::AbstractMatrix,
+    x₀,
+    howmany,
+    which,
+    alg::Lanczos
+)
+    vals, vecs, info = eigsolve(A, x₀, howmany, which, alg)
+
+    # manually truncate "howmany" values
+    vals = vals[1:howmany]
+    vecs = vecs[1:howmany]
+
+    project_A = ProjectTo(A)
+
+    function eigsolve_pullback(ΔX)
+        Δvals, Δvecs, _ = ΔX
+        ∂self = NoTangent()
+        ∂x₀ = ZeroTangent()
+        ∂alg = NoTangent()
+        ∂which = NoTangent()
+        ∂howmany = NoTangent()
+        ∂A = mapreduce(+, vals, vecs, Δvals, Δvecs) do λ, v, Δλ, Δv
+            if isa(Δv, typeof(ZeroTangent()))
+                ξ = Δv
+            else
+                alg_reverse = GMRES(;
+                    tol=alg.tol,
+                    krylovdim=alg.krylovdim,
+                    maxiter=alg.maxiter,
+                    orth=alg.orth
+                )
+                b = Δv - dot(v, Δv) * v
+                ξ, info_reverse = linsolve(A, b, zero(λ) * b, alg_reverse, -λ)
+                info_reverse.converged == 0 && @warn "Cotangent problem did not converge."
+                ξ -= dot(v, ξ) * v
+            end
+            if A isa StridedMatrix
+                return InplaceableThunk(
+                    Ā -> mul!(Ā, v, Δλ * v' - ξ', true, true),
+                    @thunk(v * (Δλ * v' - ξ')),
+                )
+            else
+                return @thunk(project_A(v * (Δλ * v' - ξ')))
+            end
+        end
+        return ∂self, ∂A, ∂x₀, ∂howmany, ∂which, ∂alg
+    end
+    return (vals, vecs, info), eigsolve_pullback
+end
+
+function ChainRulesCore.rrule(
+    config::RuleConfig{>:HasReverseMode},
+    ::typeof(eigsolve),
+    f,
+    x₀,
+    howmany,
+    which,
+    alg::Lanczos
+)
+    vals, vecs, info = eigsolve(f, x₀, howmany, which, alg)
+
+    # truncate "howmany" values
+    vals = vals[1:howmany]
+    vecs = vecs[1:howmany]
+
+    f_pullbacks = map(x -> rrule_via_ad(config, f, x)[2], vecs)
+
+    function eigsolve_pullback(ΔX)
+        Δvals, Δvecs, _ = unthunk(ΔX)
+        ∂self = NoTangent()
+        ∂x₀ = ZeroTangent()
+        ∂alg = NoTangent()
+        ∂which = NoTangent()
+        ∂howmany = NoTangent()
+        ∂f = mapreduce(+, vals, vecs, Δvals, Δvecs, f_pullbacks) do λ, v, Δλ, Δv, f_pullback
+            if isa(Δv, typeof(ZeroTangent()))
+                ξ = Δv
+            else
+                alg_reverse = GMRES(;
+                    tol=alg.tol,
+                    krylovdim=alg.krylovdim,
+                    maxiter=alg.maxiter,
+                    orth=alg.orth
+                )
+                b = axpy!(-dot(v, Δv), v, one(λ) * Δv)
+                ξ, info_reverse = linsolve(f, b, zero(λ) * v, alg_reverse, -λ)
+                info_reverse.converged == 0 && @warn "Cotangent problem did not converge."
+                axpy!(-dot(v, ξ), v, ξ)
+            end
+            axpby!(Δλ, v, -one(λ), ξ)
+            return f_pullback(ξ)[1]
+        end
+        return ∂self, ∂f, ∂x₀, ∂howmany, ∂which, ∂alg
+    end
+    return (vals, vecs, info), eigsolve_pullback
+end
+
+# correct dispatch behaviour
+function ChainRulesCore.rrule(
+    config::RuleConfig{>:HasReverseMode},
+    ::typeof(eigsolve),
+    A::AbstractMatrix,
+    x₀,
+    howmany,
+    which,
+    alg::Lanczos
+)
+    return rrule(eigsolve, A, x₀, howmany, which, alg)
+end
+
+function ChainRulesCore.rrule(
+    ::typeof(eigsolve),
+    A::AbstractMatrix,
+    x₀,
+    howmany,
+    which,
+    alg::Arnoldi
+)
+    λᵣs, rs, infoᵣ = eigsolve(A, x₀, howmany, which, alg)
+    λᵣs = λᵣs[1:howmany]
+    rs = rs[1:howmany]
+
+    # compute left eigenvectors
+    λₗs, ls, infoₗ = eigsolve(A', x₀, howmany, which, alg)
+    infoₗ.converged < howmany && @warn "Left eigenvectors not converged."
+    λₗs = λₗs[1:howmany]
+    ls = ls[1:howmany]
+    all(conj.(λₗs) .≈ λᵣs) || @warn "Left and right eigenvalues disagree."
+
+    project_A = ProjectTo(A)
+
+    function eigsolve_pullback(ΔX)
+        Δλs, Δrs, _ = ΔX
+        ∂self = NoTangent()
+        ∂x₀ = ZeroTangent()
+        ∂alg = NoTangent()
+        ∂which = NoTangent()
+        ∂howmany = NoTangent()
+        ∂A = mapreduce(+, λᵣs, rs, ls, Δλs, Δrs) do λ, r, l, Δλ, Δr
+            ϕ = dot(l, r)
+            Δr -= dot(r, Δr) * r
+            if isa(Δr, typeof(ZeroTangent()))
+                ξ = Δr
+            else
+                alg_reverse = GMRES(; tol=alg.tol, krylovdim=alg.krylovdim, maxiter=alg.maxiter, orth=alg.orth)
+                b = Δr - dot(r, Δr) / conj(ϕ) * l
+                ξ, info_reverse = linsolve(A', b, zero(λ) * b, alg_reverse, -conj(λ))
+                info_reverse.converged == 0 && @warn "Cotangent problem did not converge."
+                ξ -= dot(r, ξ) / conj(ϕ) * l
+            end
+            if A isa StridedMatrix
+                return InplaceableThunk(
+                    Ā -> mul!(Ā, Δλ / conj(ϕ) * l - ξ, r', true, true),
+                    @thunk((Δλ / conj(ϕ) * l - ξ) * r'),
+                )
+            else
+                return project_A((Δλ / conj(ϕ) * l - ξ) * r')
+            end
+        end
+        return ∂self, ∂A, ∂x₀, ∂howmany, ∂which, ∂alg
+    end
+    return (λᵣs, rs, infoᵣ), eigsolve_pullback
+end
+
+# correct dispatch behaviour
+function ChainRulesCore.rrule(
+    config::RuleConfig{>:HasReverseMode},
+    ::typeof(eigsolve),
+    A::AbstractMatrix,
+    x₀,
+    howmany,
+    which,
+    alg::Arnoldi
+)
+    return rrule(eigsolve, A, x₀, howmany, which, alg)
+end
+
+function ChainRulesCore.rrule(
+    config::RuleConfig{>:HasReverseMode},
+    ::typeof(eigsolve),
+    f,
+    x₀,
+    howmany,
+    which,
+    alg::Arnoldi
+)
+    λᵣs, rs, infoᵣ = eigsolve(f, x₀, howmany, which, alg)
+    λᵣs = λᵣs[1:howmany]
+    rs = rs[1:howmany]
+
+    # compute left eigenvectors
+    (_, f_pullback) = rrule_via_ad(config, f, x₀)
+    fᴴ(x) = f_pullback(x)[2]
+    λₗs, ls, infoₗ = eigsolve(fᴴ, x₀, howmany, which, alg)
+    infoₗ.converged < howmany && @warn "Left eigenvectors not converged."
+    λₗs = λₗs[1:howmany]
+    ls = ls[1:howmany]
+    all(conj.(λₗs) .≈ λᵣs) || @warn "Left and right eigenvalues disagree."
+
+    f_pullbacks = map(x -> rrule_via_ad(config, f, x)[2], rs)
+
+    function eigsolve_pullback(ΔX)
+        Δλs, Δrs, _ = ΔX
+        ∂self = NoTangent()
+        ∂x₀ = ZeroTangent()
+        ∂alg = NoTangent()
+        ∂which = NoTangent()
+        ∂howmany = NoTangent()
+        ∂f = mapreduce(+, λᵣs, rs, ls, Δλs, Δrs, f_pullbacks) do λ, r, l, Δλ, Δr, back
+            ϕ = dot(l, r)
+            if isa(Δr, typeof(ZeroTangent()))
+                ξ = Δr
+            else
+                axpy!(-dot(r, Δr), r, Δr)
+                alg_reverse = GMRES(;
+                    tol=alg.tol,
+                    krylovdim=alg.krylovdim,
+                    maxiter=alg.maxiter,
+                    orth=alg.orth
+                )
+                b = axpy!(-dot(r, Δr) / conj(ϕ), l, Δr)
+                ξ, info_reverse = linsolve(fᴴ, b, zero(λ) * r, alg_reverse, -conj(λ))
+                info_reverse.converged == 0 && @warn "Cotangent problem did not converge."
+                axpy!(-dot(r, ξ) / conj(ϕ), l, ξ)
+            end
+            return back(axpby!(Δλ / conj(ϕ), l, -one(Δλ), ξ))[1]
+        end
+        return ∂self, ∂f, ∂x₀, ∂howmany, ∂which, ∂alg
+    end
+    return (λᵣs, rs, infoᵣ), eigsolve_pullback
+end
--- test/ad.jl
+++ test/ad.jl
@@ -1,91 +1,216 @@
 module LinsolveAD
-    using KrylovKit, LinearAlgebra
-    using Random, Test
-    using ChainRulesCore, ChainRulesTestUtils, Zygote, FiniteDifferences
+using KrylovKit, LinearAlgebra
+using Random, Test
+using ChainRulesCore, ChainRulesTestUtils, Zygote, FiniteDifferences
 
-    fdm = ChainRulesTestUtils._fdm
-    precision(T::Type{<:Number}) = eps(real(T))^(2/3)
-    n = 10
-    N = 30
+fdm = ChainRulesTestUtils._fdm
+precision(T::Type{<:Number}) = eps(real(T))^(2 / 3)
+n = 10
+N = 30
 
-    function build_mat_example(A, b; tol = precision(eltype(A)), kwargs...)
-        Avec, A_fromvec = to_vec(A)
-        bvec, b_fromvec = to_vec(b)
-        T = eltype(A)
+function build_mat_example(A, b; tol=precision(eltype(A)), kwargs...)
+    Avec, A_fromvec = to_vec(A)
+    bvec, b_fromvec = to_vec(b)
+    T = eltype(A)
 
-        function mat_example(Av, bv)
-            A′ = A_fromvec(Av)
-            b′ = b_fromvec(bv)
-            x, info = linsolve(A′, b′, zero(b′), GMRES(; tol = tol, kwargs...))
-            info.converged == 0 && @warn "linsolve did not converge"
-            xv, = to_vec(x)
-            return xv
-        end
-        return mat_example, Avec, bvec
+    function mat_example(Av, bv)
+        A′ = A_fromvec(Av)
+        b′ = b_fromvec(bv)
+        x, info = linsolve(A′, b′, zero(b′), GMRES(; tol=tol, kwargs...))
+        info.converged == 0 && @warn "linsolve did not converge"
+        xv, = to_vec(x)
+        return xv
     end
+    return mat_example, Avec, bvec
+end
 
-    function build_fun_example(A, b, c, d, e, f; tol = precision(eltype(A)), kwargs...)
-        Avec, matfromvec = to_vec(A)
-        bvec, vecfromvec = to_vec(b)
-        cvec, = to_vec(c)
-        dvec, = to_vec(d)
-        evec, scalarfromvec = to_vec(e)
-        fvec, = to_vec(f)
+function build_fun_example(A, b, c, d, e, f; tol=precision(eltype(A)), kwargs...)
+    Avec, matfromvec = to_vec(A)
+    bvec, vecfromvec = to_vec(b)
+    cvec, = to_vec(c)
+    dvec, = to_vec(d)
+    evec, scalarfromvec = to_vec(e)
+    fvec, = to_vec(f)
 
-        function fun_example(Av, bv, cv, dv, ev, fv)
-            A′ = matfromvec(Av)
-            b′ = vecfromvec(bv)
-            c′ = vecfromvec(cv)
-            d′ = vecfromvec(dv)
-            e′ = scalarfromvec(ev)
-            f′ = scalarfromvec(fv)
+    function fun_example(Av, bv, cv, dv, ev, fv)
+        A′ = matfromvec(Av)
+        b′ = vecfromvec(bv)
+        c′ = vecfromvec(cv)
+        d′ = vecfromvec(dv)
+        e′ = scalarfromvec(ev)
+        f′ = scalarfromvec(fv)
 
-            x, info = linsolve(b′, zero(b′), GMRES(; tol = tol, kwargs...), e′, f′) do y
-                A′*y + c′ * dot(d′, y)
+        x, info = linsolve(b′, zero(b′), GMRES(; tol=tol, kwargs...), e′, f′) do y
+            A′ * y + c′ * dot(d′, y)
+        end
+        # info.converged > 0 || @warn "not converged"
+        xv, = to_vec(x)
+        return xv
+    end
+    return fun_example, Avec, bvec, cvec, dvec, evec, fvec
+end
+
+@testset "Small linsolve AD test" begin
+    @testset for T in (Float32, Float64, ComplexF32, ComplexF64)
+        A = 2 * (rand(T, (n, n)) .- one(T) / 2)
+        b = 2 * (rand(T, n) .- one(T) / 2)
+        b /= norm(b)
+
+        mat_example, Avec, bvec = build_mat_example(
+            A, b; tol=cond(A) * eps(real(T)), krylovdim=n, maxiter=1
+        )
+
+        (JA, Jb) = FiniteDifferences.jacobian(fdm, mat_example, Avec, bvec)
+        (JA′, Jb′) = Zygote.jacobian(mat_example, Avec, bvec)
+        @test JA ≈ JA′ rtol = cond(A) * precision(T)
+        @test Jb ≈ Jb′ rtol = cond(A) * precision(T)
+    end
+end
+
+@testset "Large linsolve AD test" begin
+    for T in (Float64, ComplexF64)
+        A = rand(T, (N, N)) .- one(T) / 2
+        A = I - (9 // 10) * A / maximum(abs, eigvals(A))
+        b = 2 * (rand(T, N) .- one(T) / 2)
+        c = 2 * (rand(T, N) .- one(T) / 2)
+        d = 2 * (rand(T, N) .- one(T) / 2)
+        e = rand(T)
+        f = rand(T)
+
+        fun_example, Avec, bvec, cvec, dvec, evec, fvec = build_fun_example(
+            A, b, c, d, e, f; tol=precision(T), krylovdim=20
+        )
+
+        (JA, Jb, Jc, Jd, Je, Jf) = FiniteDifferences.jacobian(
+            fdm, fun_example, Avec, bvec, cvec, dvec, evec, fvec
+        )
+        (JA′, Jb′, Jc′, Jd′, Je′, Jf′) = Zygote.jacobian(
+            fun_example, Avec, bvec, cvec, dvec, evec, fvec
+        )
+        @test JA ≈ JA′
+        @test Jb ≈ Jb′
+        @test Jc ≈ Jc′
+        @test Jd ≈ Jd′
+        @test Je ≈ Je′
+        @test Jf ≈ Jf′
+    end
+end
+end
+
+module EigsolveAD
+using KrylovKit, LinearAlgebra
+using Random, Test
+using ChainRulesCore, ChainRulesTestUtils, Zygote
+
+precision(T::Type{<:Number}) = eps(real(T))^(2 / 3)
+n = 2
+
+@testset "Lanczos - eigsolve AD full" begin
+    @testset for T in (Float32, Float64, ComplexF32, ComplexF64)
+        A = rand(T, (n, n)) .- one(T) / 2
+        A = Hermitian((A + A') / 2)
+        v = rand(T, (n,))
+        alg = Lanczos(; krylovdim=2 * n, maxiter=1, tol=precision(T))
+        which = :LM
+        function f(A)
+            vals, vecs, info = eigsolve(A, v, n, which, alg)
+
+            vecs_phased = map(vecs) do vec
+                return vec ./ exp(angle(vec[1])im)
             end
-            # info.converged > 0 || @warn "not converged"
-            xv, = to_vec(x)
-            return xv
+            D = vcat(vals...)
+            U = hcat(vecs_phased...)
+            return D, U
         end
-        return fun_example, Avec, bvec, cvec, dvec, evec, fvec
-    end
 
-    @testset "Small linsolve AD test" begin
-        @testset for T in (Float32, Float64, ComplexF32, ComplexF64)
-            A = 2*(rand(T, (n,n)) .- one(T)/2)
-            b = 2*(rand(T, n) .- one(T)/2)
-            b /= norm(b)
-
-            mat_example, Avec, bvec = build_mat_example(A, b; tol = cond(A)*eps(real(T)), krylovdim = n, maxiter = 1)
-
-            (JA, Jb) = FiniteDifferences.jacobian(fdm, mat_example, Avec, bvec)
-            (JA′, Jb′) = Zygote.jacobian(mat_example, Avec, bvec)
-            @test JA ≈ JA′ rtol=cond(A)*precision(T)
-            @test Jb ≈ Jb′ rtol=cond(A)*precision(T)
+        function g(A)
+            vals, vecs = eigen(A; sortby=x -> -abs(x))
+            vecs_phased = map(1:size(vecs, 2)) do i
+                return vecs[:, i] ./ exp(angle(vecs[1, i])im)
+            end
+            return vals, hcat(vecs_phased...)
         end
-    end
 
-    @testset "Large linsolve AD test" begin
-        for T in (Float64, ComplexF64)
-            A = rand(T,(N,N)) .- one(T)/2
-            A = I-(9//10)*A/maximum(abs, eigvals(A))
-            b = 2*(rand(T, N) .- one(T)/2)
-            c = 2*(rand(T, N) .- one(T)/2)
-            d = 2*(rand(T, N) .- one(T)/2)
-            e = rand(T)
-            f = rand(T)
+        function h(A)
+            vals, vecs = eigsolve(v, n, which, alg) do x
+                return A * x
+            end
+            vecs_phased = map(vecs) do vec
+                return vec ./ exp(angle(vec[1])im)
+            end
+            return vcat(vals...), hcat(vecs_phased...)
+        end
 
-            fun_example, Avec, bvec, cvec, dvec, evec, fvec = build_fun_example(A, b, c, d, e, f; tol = precision(T), krylovdim = 20)
+        y1, back1 = pullback(f, A)
+        y2, back2 = pullback(g, A)
+        y3, back3 = pullback(h, A)
 
-            (JA, Jb, Jc, Jd, Je, Jf) = FiniteDifferences.jacobian(fdm, fun_example,
-                Avec, bvec, cvec, dvec, evec, fvec)
-            (JA′, Jb′, Jc′, Jd′, Je′, Jf′) = Zygote.jacobian(fun_example, Avec, bvec, cvec, dvec, evec, fvec)
-            @test JA ≈ JA′
-            @test Jb ≈ Jb′
-            @test Jc ≈ Jc′
-            @test Jd ≈ Jd′
-            @test Je ≈ Je′
-            @test Jf ≈ Jf′
+        for i in 1:2
+            @test y1[i] ≈ y2[i]
+            @test y2[i] ≈ y3[i]
+        end
+
+        for i in 1:3
+            Δvals = rand(T, (n,))
+            Δvecs = rand(T, (n, n))
+            @test first(back1((Δvals, Δvecs))) ≈ first(back2((Δvals, Δvecs)))
+            @test first(back2((Δvals, Δvecs))) ≈ first(back3((Δvals, Δvecs)))
         end
     end
+end
+
+@testset "Arnoldi - eigsolve AD full" begin
+    @testset for T in (Float32, Float64, ComplexF32, ComplexF64)
+        A = rand(T, (n, n)) .- one(T) / 2
+        v = rand(T, (n,))
+
+        alg = Arnoldi(; krylovdim=2 * n, maxiter=1, tol=precision(T))
+        which = :LM
+
+        function f(A)
+            vals, vecs, _ = eigsolve(A, v, n, which, alg)
+
+            vecs_phased = map(vecs) do vec
+                return vec ./ exp(angle(vec[1])im)
+            end
+            D = vcat(vals...)
+            U = hcat(vecs_phased...)
+            return D, U
+        end
+
+        function g(A)
+            vals, vecs = eigen(A; sortby=x -> -abs(x))
+            vecs_phased = map(1:size(vecs, 2)) do i
+                return vecs[:, i] ./ exp(angle(vecs[1, i])im)
+            end
+            return vals, hcat(vecs_phased...)
+        end
+
+        function h(A)
+            vals, vecs, _ = eigsolve(v, n, which, alg) do x
+                return A * x
+            end
+            vecs_phased = map(vecs) do vec
+                return vec ./ exp(angle(vec[1])im)
+            end
+            return vcat(vals...), hcat(vecs_phased...)
+        end
+
+        y1, back1 = pullback(f, A)
+        y2, back2 = pullback(g, A)
+        y3, back3 = pullback(h, A)
+
+        for i in 1:2
+            @test y1[i] ≈ y2[i]
+            @test y2[i] ≈ y3[i]
+        end
+
+        for i in 1:3
+            Δvals = rand(T, (n,))
+            Δvecs = rand(T, (n, n))
+            @test first(back1((Δvals, Δvecs))) ≈ first(back2((Δvals, Δvecs)))
+            @test first(back2((Δvals, Δvecs))) ≈ first(back3((Δvals, Δvecs)))
+        end
+    end
+end
 end
