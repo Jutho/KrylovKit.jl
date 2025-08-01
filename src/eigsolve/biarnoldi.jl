35fa97eabef5430a9ee88115480d1322edd46fd7
Tim Pokart <tipfom@users.noreply.github.com>
2025-08-01 09:51:56 +0200

    alternative `@constinferred` fix

--- src/eigsolve/biarnoldi.jl
+++ src/eigsolve/biarnoldi.jl
@@ -167,18 +167,14 @@ function bieigsolve(f, v₀, w₀, howmany::Int, which::Selector, alg::BiArnoldi
     # Construct the actual eigenvectors and residuals
     VS = view(Q, :, 1:howmany′) * vecsS
     vectorsS = [V * v for v in cols(VS)]
-    hᴴVS = vec(h[1:howmany′]' * vecsS)
-    residualsS = [scale(rV, s) for s in hᴴVS]
-    normresidualsS = let βrV = norm(rV)
-        [abs(βrV * s) for s in hᴴVS]
-    end
+    hᴴVS = map(Base.Fix1(dot, h[1:howmany′]), cols(vecsS))
+    residualsS = map(Base.Fix1(scale, rV), hᴴVS)::typeof(vectorsS)
+    normresidualsS = map(abs ∘ Base.Fix1(*, norm(rV)), hᴴVS)
     VT = view(Z, :, 1:howmany′) * vecsT
     vectorsT = [W * v for v in cols(VT)]
-    kᴴVT = vec(k[1:howmany′]' * vecsT)
-    residualsT = [scale(rW, s) for s in kᴴVT]
-    normresidualsT = let βrW = norm(rW)
-        [abs(βrW * s) for s in kᴴVT]
-    end
+    hᴴVT = map(Base.Fix1(dot, k[1:howmany′]), cols(vecsT))
+    residualsT = map(Base.Fix1(scale, rW), hᴴVT)::typeof(vectorsT)
+    normresidualsT = map(abs ∘ Base.Fix1(*, norm(rW)), hᴴVT)
 
     if (converged < howmany) && alg.verbosity >= WARN_LEVEL
         @warn """BiArnoldi eigsolve stopped without convergence after $numiter iterations:
