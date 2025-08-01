8788b3c52970205e5768adc36630df455fcf43d3
Jutho Haegeman <jutho.haegeman@ugent.be>
2025-08-01 14:00:02 +0200

    add BiArnoldi docs

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -247,6 +247,26 @@ function Arnoldi(;
     return Arnoldi(orth, krylovdim, maxiter, tol, eager, verbosity)
 end
 
+"""
+    BiArnoldi(; krylovdim=KrylovDefaults.krylovdim[],
+              maxiter=KrylovDefaults.maxiter[],
+              tol=KrylovDefaults.tol[],
+              orth=KrylovDefaults.orth,
+              eager=false,
+              verbosity=KrylovDefaults.verbosity[])
+
+Represents the BiArnoldi algorithm for building the Krylov subspace for a general matrix or
+linear operator. Can be used in `bieigsolve`. The corresponding algorithm will build a Krylov subspace of size at most `krylovdim`, which will be repeated at most `maxiter` times and
+will stop when the norm of the residual of the Arnoldi factorization is smaller than `tol`.
+The orthogonalizer `orth` will be used to orthogonalize the different Krylov vectors.
+Eager mode, as selected by `eager=true`, means that the algorithm that uses this BiArnoldi
+process (i.e. `bieigsolve`) can try to finish its computation before the total Krylov
+subspace of dimension `krylovdim` is constructed. The default verbosity level
+`verbosity` amounts to printing warnings upon lack of convergence.
+
+
+See also: [`bieigsolve`](@ref), [`Orthogonalizer`](@ref)
+"""
 struct BiArnoldi{O<:Orthogonalizer,S<:Real} <: KrylovAlgorithm
     orth::O
     krylovdim::Int
