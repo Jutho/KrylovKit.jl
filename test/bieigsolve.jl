a75b5ea9d831d4d242b61e29dd350fe3aa84f186
Tim Pokart <tipfom@users.noreply.github.com>
2025-08-01 09:10:42 +0200

    added eager testset

--- test/bieigsolve.jl
+++ test/bieigsolve.jl
@@ -112,82 +112,122 @@ end
     scalartypes = mode === :vector ? (Float32, Float64, ComplexF32, ComplexF64) :
                   (ComplexF64,)
     orths = mode === :vector ? (cgs2, mgs2, cgsr, mgsr) : (mgsr,)
+    @testset for eager in (true, false)
+        @testset for T in scalartypes
+            @testset for orth in orths
+                A = rand(T, (N, N)) .- one(T) / 2
+                v = rand(T, (N,))
+                w = rand(T, (N,))
+                alg = BiArnoldi(; krylovdim=3 * n, maxiter=40,
+                                tol=tolerance(T), verbosity=SILENT_LEVEL)
+                #! format: off
+                D1, (V1, W1), (infoV1, infoW1) = @constinferred bieigsolve(
+                    wrapop(A, Val(mode)), wrapvec(v, Val(mode)), wrapvec(w, Val(mode)), 
+                    n, :SR, alg)
+                D2, (V2, W2), (infoV2, infoW2) = bieigsolve(
+                    wrapop(A, Val(mode)), wrapvec(v, Val(mode)), wrapvec(w, Val(mode)), 
+                    n, :LR, alg)
+                D3, (V3, W3), (infoV3, infoW3) = bieigsolve(
+                    wrapop(A, Val(mode)), wrapvec(v, Val(mode)), wrapvec(w, Val(mode)), 
+                    n, :LM, alg)
+                #! format: on
+                D = sort(eigvals(A); by=imag, rev=true)
+
+                @test infoV1.converged == infoW1.converged
+                @test infoV2.converged == infoW2.converged
+                @test infoV3.converged == infoW3.converged
+
+                l1 = infoV1.converged
+                l2 = infoV2.converged
+                l3 = infoV3.converged
+                @test l1 > 0
+                @test l2 > 0
+                @test l3 > 0
+                @test D1[1:l1] ≊ sort(D; alg=MergeSort, by=real)[1:l1]
+                @test D2[1:l2] ≊ sort(D; alg=MergeSort, by=real, rev=true)[1:l2]
+                # sorting by abs does not seem very reliable if two distinct eigenvalues are close
+                # in absolute value, so we perform a second sort afterwards using the real part
+                @test D3[1:l3] ≊ sort(D; by=abs, rev=true)[1:l3]
+
+                UV1 = stack(unwrapvec, V1)
+                UV2 = stack(unwrapvec, V2)
+                UV3 = stack(unwrapvec, V3)
+                RV1 = stack(unwrapvec, infoV1.residual)
+                RV2 = stack(unwrapvec, infoV2.residual)
+                RV3 = stack(unwrapvec, infoV3.residual)
+
+                UW1 = stack(unwrapvec, W1)
+                UW2 = stack(unwrapvec, W2)
+                UW3 = stack(unwrapvec, W3)
+                RW1 = stack(unwrapvec, infoW1.residual)
+                RW2 = stack(unwrapvec, infoW2.residual)
+                RW3 = stack(unwrapvec, infoW3.residual)
+
+                @test A * UV1 ≈ UV1 * Diagonal(D1) + RV1
+                @test A' * UW1 ≈ UW1 * Diagonal(D1)' + RW1
+                @test A * UV2 ≈ UV2 * Diagonal(D2) + RV2
+                @test A' * UW2 ≈ UW2 * Diagonal(D2)' + RW2
+                @test A * UV3 ≈ UV3 * Diagonal(D3) + RV3
+                @test A' * UW3 ≈ UW3 * Diagonal(D3)' + RW3
+
+                if T <: Complex
+                    #! format: off
+                    D1, (V1, W1), (infoV1, infoW1) = bieigsolve(
+                        wrapop(A, Val(mode)), wrapvec(v, Val(mode)), wrapvec(w, Val(mode)), 
+                        n, :SI, alg)
+                    D2, (V2, W2), (infoV2, infoW2) = bieigsolve(
+                        wrapop(A, Val(mode)), wrapvec(v, Val(mode)), wrapvec(w, Val(mode)), 
+                        n, :LI, alg)
+                    #! format: on
+
+                    @test infoV1.converged == infoW1.converged
+                    @test infoV2.converged == infoW2.converged
+
+                    l1 = infoV1.converged
+                    l2 = infoV2.converged
+                    @test l1 > 0
+                    @test l2 > 0
+                    @test D1[1:l1] ≈ sort(D; by=imag)[1:l1]
+                    @test D2[1:l2] ≈ sort(D; by=imag, rev=true)[1:l2]
+
+                    UV1 = stack(unwrapvec, V1)
+                    UV2 = stack(unwrapvec, V2)
+                    RV1 = stack(unwrapvec, infoV1.residual)
+                    RV2 = stack(unwrapvec, infoV2.residual)
+
+                    UW1 = stack(unwrapvec, W1)
+                    UW2 = stack(unwrapvec, W2)
+                    RW1 = stack(unwrapvec, infoW1.residual)
+                    RW2 = stack(unwrapvec, infoW2.residual)
+
+                    @test A * UV1 ≈ UV1 * Diagonal(D1) + RV1
+                    @test A' * UW1 ≈ UW1 * Diagonal(D1)' + RW1
+                    @test A * UV2 ≈ UV2 * Diagonal(D2) + RV2
+                    @test A' * UW2 ≈ UW2 * Diagonal(D2)' + RW2
+                end
+            end
+        end
+    end
+end
+
+@testset "BiArnoldi - shortened api ($mode)" for mode in (:vector,)
+    scalartypes = mode === :vector ? (Float32, Float64, ComplexF32, ComplexF64) :
+                  (ComplexF64,)
+    orths = mode === :vector ? (cgs2, mgs2, cgsr, mgsr) : (mgsr,)
     @testset for T in scalartypes
         @testset for orth in orths
-            A = rand(T, (N, N)) .- one(T) / 2
-            v = rand(T, (N,))
-            w = rand(T, (N,))
-            alg = BiArnoldi(; krylovdim=3 * n, maxiter=2, orth = orth,
-                            tol=tolerance(T), eager=true, verbosity=EACHITERATION_LEVEL)
-            #! format: off
-            D1, (V1, W1), (infoV1, infoW1) =
-                @constinferred bieigsolve(wrapop(A, Val(mode)),
-                                                 wrapvec(v, Val(mode)), wrapvec(w, Val(mode)),
-                                                 n, :SR, alg)
-            #! format: on
-            D2, (V2, W2), (infoV2, infoW2) = bieigsolve(wrapop(A, Val(mode)),
-                                                        wrapvec(v, Val(mode)),
-                                                        wrapvec(w, Val(mode)),
-                                                        n, :LR, alg)
-            D3, (V3, W3), (infoV3, infoW3) = bieigsolve(wrapop(A, Val(mode)),
-                                                       wrapvec(v, Val(mode)),
-                                                       wrapvec(w, Val(mode)),
-                                                       n, :LM, alg)
-            D = sort(eigvals(A); by=imag, rev=true)
+            A = rand(T, (n, n)) .- one(T) / 2
+            v = rand(T, (n,))
+            w = rand(T, (n,))
+            n1 = div(n, 2)
 
-            l1 = infoV1.converged
-            l2 = infoV2.converged
-            l3 = infoV3.converged
-            @test l1 > 0
-            @test l2 > 0
-            @test l3 > 0
-            @test D1[1:l1] ≊ sort(D; alg=MergeSort, by=real)[1:l1]
-            @test D2[1:l2] ≊ sort(D; alg=MergeSort, by=real, rev=true)[1:l2]
-            # sorting by abs does not seem very reliable if two distinct eigenvalues are close
-            # in absolute value, so we perform a second sort afterwards using the real part
-            @test D3[1:l3] ≊ sort(D; by=abs, rev=true)[1:l3]
+            @constinferred bieigsolve(wrapop(A, Val(mode)), wrapvec(v, Val(mode)),
+                                      wrapvec(w, Val(mode)),
+                                      n1; orth=orth, krylovdim=n, maxiter=1,
+                                      tol=tolerance(T))
 
-            U1 = stack(unwrapvec, V1)
-            U2 = stack(unwrapvec, V2)
-            U3 = stack(unwrapvec, V3)
-            R1 = stack(unwrapvec, infoV1.residual)
-            R2 = stack(unwrapvec, infoV2.residual)
-            R3 = stack(unwrapvec, infoV3.residual)
-            @test A * U1 ≈ U1 * Diagonal(D1) + R1
-            @test A * U2 ≈ U2 * Diagonal(D2) + R2
-            @test A * U3 ≈ U3 * Diagonal(D3) + R3
-
-            U1 = stack(unwrapvec, W1)
-            U2 = stack(unwrapvec, W2)
-            U3 = stack(unwrapvec, W3)
-            R1 = stack(unwrapvec, infoW1.residual)
-            R2 = stack(unwrapvec, infoW2.residual)
-            R3 = stack(unwrapvec, infoW3.residual)
-            @test A' * U1 ≈ U1 * Diagonal(D1) + R1
-            @test A' * U2 ≈ U2 * Diagonal(D2) + R2
-            @test A' * U3 ≈ U3 * Diagonal(D3) + R3
-
-            # if T <: Complex
-            #     D1, V1, info1 = eigsolve(wrapop(A, Val(mode)), wrapvec(v, Val(mode)), n,
-            #                              :SI, alg)
-            #     D2, V2, info2 = eigsolve(wrapop(A, Val(mode)), wrapvec(v, Val(mode)), n,
-            #                              :LI, alg)
-            #     D = eigvals(A)
-
-            #     l1 = info1.converged
-            #     l2 = info2.converged
-            #     @test l1 > 0
-            #     @test l2 > 0
-            #     @test D1[1:l1] ≈ sort(D; by=imag)[1:l1]
-            #     @test D2[1:l2] ≈ sort(D; by=imag, rev=true)[1:l2]
-
-            #     U1 = stack(unwrapvec, V1)
-            #     U2 = stack(unwrapvec, V2)
-            #     R1 = stack(unwrapvec, info1.residual)
-            #     R2 = stack(unwrapvec, info2.residual)
-            #     @test A * U1 ≈ U1 * Diagonal(D1) + R1
-            #     @test A * U2 ≈ U2 * Diagonal(D2) + R2
-            # end
+            @constinferred bieigsolve(wrapop(A, Val(mode)), n, n1;
+                                      orth=orth, krylovdim=n, maxiter=1, tol=tolerance(T))
         end
     end
 end
